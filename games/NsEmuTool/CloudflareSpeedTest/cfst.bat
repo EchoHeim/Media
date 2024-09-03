52f09756ccaf3748bfae6314aac79b27cd4ff6b4
MacLodge <shilong.native@foxmail.com>
2024-09-03 13:43:25 +0800

    游戏模拟器归档

---
+++ games/NsEmuTool/CloudflareSpeedTest/cfst.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d D:\Recreation\_Media_\games\NsEmuTool\CloudflareSpeedTest
+CloudflareST.exe -p 0 -url "https://cloudflaremirrors.com/archlinux/images/latest/Arch-Linux-x86_64-basic.qcow2"
